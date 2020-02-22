e3718d1f5e4dc9e1189980f8d169d1821b65a6ed
Márk Bartha <mark.bartha@lombiq.com>
2020-02-22 10:40:41 +0100

    Adding bootstrap-vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BootstrapTest/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import BootstrapTest from '@/components/BootstrapTest.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    BootstrapTest
   }
 }
 </script>
